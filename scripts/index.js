245504213d7fc7fcf4451f60c22fc6ecbc9b7cd8
João Victor <victorps.joao@gmail.com>
2020-12-11 18:00:06 -0300

    Projeto ip address tracker, finalizado.

---
+++ scripts/index.js
@@ -0,0 +1,60 @@
+function search() {
+    let ipOrDomain = document.getElementById('search').value;
+
+    axios.get(`https://geo.ipify.org/api/v1?apiKey=at_cYstAyb90AdkYmKc8xZMu1gpeMtTw&domain=${ipOrDomain}`).then(res => {
+        const { ip, isp } = res.data;
+        const { city, region, timezone, postalCode, lat, lng } = res.data.location;
+
+        replace(city, region, postalCode, ip, isp, timezone, lat, lng);
+    }).catch(err => {
+        alert('IP address or domain invalid');
+    });
+}
+
+function getAddress() {
+    axios.get(`https://geo.ipify.org/api/v1?apiKey=at_cYstAyb90AdkYmKc8xZMu1gpeMtTw`).then(res => {
+        const { ip, isp } = res.data;
+        const { city, region, timezone, postalCode, lat, lng } = res.data.location;
+
+        replace(city, region, postalCode, ip, isp, timezone, lat, lng);
+    }).catch(err => console.log(err));
+
+}
+
+function replace(city, region, postalCode, ip, isp, timezone, lat, lng) {
+    let inputIp = document.getElementById('search');
+    let htmlIp = document.getElementById('ip');
+    let htmlIsp = document.getElementById('isp');
+    let htmlTimezone = document.getElementById('timezone');
+    let htmlLocation = document.getElementById('location');
+
+    htmlIp.innerHTML = ip;
+    htmlLocation.innerHTML = `${city}, ${region} ${postalCode}`;
+    htmlTimezone.innerHTML = timezone;
+    htmlIsp.innerHTML = isp;
+    inputIp.value = ip;
+
+    if(map) {
+        map.remove();
+    }
+    
+    map = L.map(document.getElementById('mapDIV'), {
+        center: [lat + 0.002, lng],
+        zoom: 16
+    });
+    basemap = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    });
+    basemap.addTo(map);
+
+    L.marker([lat, lng]).addTo(map)
+    .bindPopup(`${isp}.<br> ${postalCode}`)
+    .openPopup();
+}
+
+let map;
+let basemap;
+
+window.onload = () => {
+    getAddress();
+
+}; 
